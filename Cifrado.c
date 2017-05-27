1d2b95d8c0de3a01e8033069ef0d82d9785b6c21
Israel <israelcanog7@gmail.com>
2017-05-26 21:57:20 -0500

    2da Completa

--- Cifrado.c
+++ Cifrado.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 char *cifrar(char *mensaje,int n){
     int i,j;
     for(i=0;i<strlen(mensaje);i++){
@@ -25,7 +26,32 @@ char *cifrar(char *mensaje,int n){
     return mensaje;
 }
 
+char *cifrarMorse(char *mensaje){
+    char *tabla[36]={".-", "-...", "-.-.", "-..", "." , "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                         "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---",
+                         "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+    int i,j;
+    for(i=0;i<strlen(mensaje);i++){
+    	if((mensaje[i]>=97&&mensaje[i]<=122)){
+		printf("%s ", tabla[mensaje[i]-97]);
+	}else if((mensaje[i]>=65&&mensaje[i]<=90)){
+		printf("%s ", tabla[mensaje[i]-65]);
+	}else if (mensaje[i]==32){
+		printf("/ ");
+	}else if((mensaje[i]>=48&&mensaje[i]<=57)){
+		printf("%s ", tabla[mensaje[i]-22]);
+	}
+    }
+	return mensaje;
+}
+
+
 int main(int argc, char * argv[]){
+
+	//char *tabla[36]={".-", "-...", "-.-.", "-..", "." , "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+	//		 "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---",
+	//		 "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
+
 	int numero;
 	if(argc==1){
 		printf("\n\t\t========================================\n\n\tIngrese mensaje a cifrar: ");
@@ -37,9 +63,9 @@ int main(int argc, char * argv[]){
 		numero=strtol(argv[1], NULL, 10);
 	}
 	*cifrar(argv[2],numero);
-
-	printf("\tMensaje cifrado: %s\n\n\t\t========================================\n\n",argv[2]);
-	//printf("%d\n", valor);
-
+	printf("\tMensaje cifrado: %s",argv[2]);
+	printf("\tMensaje cifrado en morse: ");
+	*cifrarMorse(argv[2]);
+	printf("\n\n\t\t========================================\n\n");
 return 0;
 }
